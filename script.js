83c4a94ca62ad5d4147fb3872d0cee7caa186517
baehl <baehl44@yahoo.fr>
2025-01-21 15:28:23 +0100

    etape 1 terminee

---
+++ script.js
@@ -0,0 +1,54 @@
+const dossierPrincipal = {
+  nom: "Ada",
+  contenu: [
+    {
+      nom: "Projets collectifs Ada",
+      contenu: [
+        {
+          nom: "Pico8",
+          contenu: [
+            {
+              nom: "mariokart.p8",
+            },
+          ],
+        },
+        {
+          nom: "Dataviz",
+          contenu: [
+            {
+              nom: "index.html",
+            },
+            {
+              nom: "script.js",
+            },
+          ],
+        },
+      ],
+    },
+    { nom: "CV.pdf" },
+    {
+      nom: "Projets persos",
+      contenu: [
+        {
+          nom: "Portfolio",
+          contenu: [
+            {
+              nom: "index.html",
+            },
+            {
+              nom: "script.js",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+//Etape 1: fonction afficherDossier qui va afficher le nom du dossier principal
+//en utilisant la variable dossierPrincipal
+function afficherDossier(dossierPrincipal) {
+   
+  console.log(`🗂️ ${dossierPrincipal.nom}`);
+}
+afficherDossier(dossierPrincipal);
+
